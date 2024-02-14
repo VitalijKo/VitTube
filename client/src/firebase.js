cd6f5ee1eec88bac69b3cce3383a609b3c9ddcd1
VitalijKo <vitalijko4805go@gmail.com>
2024-02-14 15:00:28 +0400

    Update firebase.js

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -2,12 +2,13 @@ import { initializeApp } from 'firebase/app';
 import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_KEY,
-  authDomain: '',
-  projectId: '',
-  storageBucket: '',
-  messagingSenderId: '',
-  appId: ''
+  apiKey: 'AIzaSyDuW_ZUNtTmO_wOTnDmMBhxxGXq4IXYlMo',
+  authDomain: 'vittube-4d7e5.firebaseapp.com',
+  projectId: 'vittube-4d7e5',
+  storageBucket: 'vittube-4d7e5.appspot.com',
+  messagingSenderId: '606928656032',
+  appId: '1:606928656032:web:2992f86373b83261dadbc5',
+  measurementId: 'G-3CVYP6EWR1'
 };
 
 const app = initializeApp(firebaseConfig);
